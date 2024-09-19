d44eff9ab3cc230cbc398ec3afae2582ab91a094
PeterToth <petertortel@hotmail.com>
2024-09-19 19:16:12 +0200

    core: HeroKnight format

--- Assets/ThirdParty/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/ThirdParty/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroKnight : MonoBehaviour {
+public class HeroKnight : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
-    [SerializeField] float      m_rollForce = 6.0f;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_rollForce = 6.0f;
     [SerializeField] public bool double_jump_enabled = false;
     [SerializeField] public bool can_double_jump = false;
     [SerializeField] public bool wall_jump_enabled = false;
@@ -16,7 +17,7 @@ public class HeroKnight : MonoBehaviour {
 
     [SerializeField] public bool m_blocking = false;
 
-    [SerializeField] public LayerMask enemyLayers ;
+    [SerializeField] public LayerMask enemyLayers;
 
     [SerializeField] bool m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
@@ -24,32 +25,32 @@ public class HeroKnight : MonoBehaviour {
     [SerializeField] public GameObject m_PlayerActiveWeapon;
     [SerializeField] public GameObject[] m_PlayerWeapons;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_HeroKnight   m_groundSensor;
-    private Sensor_HeroKnight   m_wallSensorR1;
-    private Sensor_HeroKnight   m_wallSensorR2;
-    private Sensor_HeroKnight   m_wallSensorL1;
-    private Sensor_HeroKnight   m_wallSensorL2;
-    private bool                m_isWallSliding = false;
-    public  bool                m_grounded = false;
-    private bool                m_rolling = false;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_HeroKnight m_groundSensor;
+    private Sensor_HeroKnight m_wallSensorR1;
+    private Sensor_HeroKnight m_wallSensorR2;
+    private Sensor_HeroKnight m_wallSensorL1;
+    private Sensor_HeroKnight m_wallSensorL2;
+    private bool m_isWallSliding = false;
+    public bool m_grounded = false;
+    private bool m_rolling = false;
 
 
     //does the player look left or right
-    private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
-    private float               m_rollDuration = 8.0f / 14.0f;
-    private float               m_rollCurrentTime;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
+    private float m_rollDuration = 8.0f / 14.0f;
+    private float m_rollCurrentTime;
 
-    
+
 
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
@@ -59,36 +60,34 @@ public class HeroKnight : MonoBehaviour {
         m_wallSensorL1 = transform.Find("WallSensor_L1").GetComponent<Sensor_HeroKnight>();
         m_wallSensorL2 = transform.Find("WallSensor_L2").GetComponent<Sensor_HeroKnight>();
         enemyLayers = LayerMask.GetMask("Enemy");
-     }
+    }
 
     // Update is called once per frame
-    
-    void Update ()
+
+    void Update()
     {
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
 
         // Increase timer that checks roll duration
-        if(m_rolling)
-            m_rollCurrentTime += Time.deltaTime;
+        if (m_rolling) m_rollCurrentTime += Time.deltaTime;
 
         //clear attack flag
-        if(m_attacking && 
-            m_timeSinceAttack > m_PlayerActiveWeapon.GetComponent<WeaponScript>().attackCoolDown)
-            {
-                m_attacking = false;
-            }
+        if (m_attacking && m_timeSinceAttack > m_PlayerActiveWeapon.GetComponent<WeaponScript>().attackCoolDown)
+        {
+            m_attacking = false;
+        }
 
         // Disable rolling if timer extends duration
-        if(m_rollCurrentTime > m_rollDuration)
-            m_rolling = false;
+        if (m_rollCurrentTime > m_rollDuration) m_rolling = false;
 
         //Check if character just landed on the ground
         if (!m_grounded && m_groundSensor.State())
         {
             m_grounded = true;
             m_animator.SetBool("Grounded", m_grounded);
-            if(double_jump_enabled){
+            if (double_jump_enabled)
+            {
                 can_double_jump = m_grounded;
             }
         }
@@ -109,7 +108,7 @@ public class HeroKnight : MonoBehaviour {
             GetComponent<SpriteRenderer>().flipX = false;
             m_facingDirection = 1;
         }
-            
+
         else if (inputX < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
@@ -117,7 +116,7 @@ public class HeroKnight : MonoBehaviour {
         }
 
         // Move
-        if (!m_rolling )
+        if (!m_rolling)
             m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
 
         //Set AirSpeed in animator
@@ -128,7 +127,8 @@ public class HeroKnight : MonoBehaviour {
         m_isWallSliding = ((m_wallSensorR1.State() && m_wallSensorR2.State()) || (m_wallSensorL1.State() && m_wallSensorL2.State()))
         && !m_grounded;
         m_animator.SetBool("WallSlide", m_isWallSliding);
-        if(m_isWallSliding){
+        if (m_isWallSliding)
+        {
             Debug.Log("WallSliding");
         }
         //Death
@@ -137,24 +137,25 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetBool("noBlood", m_noBlood);
             m_animator.SetTrigger("Death");
         }
-            
+
         //Hurt
         else if (Input.GetKeyDown("q") && !m_rolling)
             m_animator.SetTrigger("Hurt");
 
         //Attack
-        else if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 
+        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack >
                 m_PlayerActiveWeapon.GetComponent<WeaponScript>().attackCoolDown &&
                 !m_rolling)
         {
             Debug.Log("Attacking");
             //reduce stamina
-            if(m_PlayerActiveWeapon.GetComponent<WeaponScript>().staminaCost <= 
-                GetComponent<CharacterStats>().GetCurrentStamina()){
-                    
+            if (m_PlayerActiveWeapon.GetComponent<WeaponScript>().staminaCost <=
+                GetComponent<CharacterStats>().GetCurrentStamina())
+            {
+
                 int maxStamina = GetComponent<CharacterStats>().GetMaxStamina();
                 int currentStamina = GetComponent<CharacterStats>().GetCurrentStamina();
-                int staminaCost = m_PlayerActiveWeapon.GetComponent<WeaponScript>().staminaCost;    
+                int staminaCost = m_PlayerActiveWeapon.GetComponent<WeaponScript>().staminaCost;
                 GetComponent<CharacterStats>().SetCurrentStamina(currentStamina - staminaCost);
 
 
@@ -175,7 +176,8 @@ public class HeroKnight : MonoBehaviour {
                 // Reset timer
                 m_timeSinceAttack = 0.0f;
             }
-            else{
+            else
+            {
                 return;
             }
 
@@ -183,14 +185,15 @@ public class HeroKnight : MonoBehaviour {
         }
 
         // Block
-        else if (Input.GetMouseButtonDown(1) && !m_rolling )
+        else if (Input.GetMouseButtonDown(1) && !m_rolling)
         {
             m_blocking = true;
             m_animator.SetTrigger("Block");
             m_animator.SetBool("IdleBlock", true);
         }
 
-        else if (Input.GetMouseButtonUp(1)){
+        else if (Input.GetMouseButtonUp(1))
+        {
             m_blocking = false;
             m_animator.SetBool("IdleBlock", false);
         }
@@ -202,12 +205,12 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetTrigger("Roll");
             m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
         }
-            
+
 
         //Jump
         else if (
-                 Input.GetKeyDown("space") && 
-                 (m_grounded|| m_isWallSliding || (double_jump_enabled && can_double_jump )) &&
+                 Input.GetKeyDown("space") &&
+                 (m_grounded || m_isWallSliding || (double_jump_enabled && can_double_jump)) &&
                  !m_rolling
                 )
         {
@@ -217,13 +220,14 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetBool("Grounded", m_grounded);
             m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
             m_groundSensor.Disable(0.2f);
-            if(!prev_grounded){
+            if (!prev_grounded)
+            {
                 can_double_jump = false;
             }
         }
 
         //Run
-        else if (Mathf.Abs(inputX) > Mathf.Epsilon )
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
         {
             // Reset timer
             m_delayToIdle = 0.05f;
@@ -235,8 +239,8 @@ public class HeroKnight : MonoBehaviour {
         {
             // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
-                if(m_delayToIdle < 0)
-                    m_animator.SetInteger("AnimState", 0);
+            if (m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
         }
     }
 
